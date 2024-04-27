c1dd013dc2409507ae2f3ccbfc11af4151d281f4
Mio Łuczak <mioluczak@gmail.com>
2024-04-27 15:58:31 +0200

    Wykonanie zadania: Tworzenie Tablic, Iteracje po tablicy

---
+++ script.js
@@ -0,0 +1,14 @@
+//  Zadanie 1: 
+//  Tworzenie Tablic
+const fruits = ['Jabłko', 'Banan', 'Pomarańcza', 'Winogrono', 'Arbuz'];
+console.log(fruits);
+
+//  Zadanie 2: 
+//  Iteracja po Tablicach
+const numbers = [42, 18, 64, 12, 57, 48, 7, 0, 92, 81, 61, 73, 77, 46, 2, 4, 5, 1, -2, -10, -245];
+
+for (number of numbers) {
+        if (number > 5) {
+            console.log(number);
+        };
+    };
